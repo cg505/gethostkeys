0e57029906482acdc7ed0f52c4b0c2eb87029da5
Christopher Cooper <christopher.c.cooper@gmail.com>
2019-01-11 23:36:05 -0800

    don't hardcode home directory, and check for `Include hkconfig`

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,54 @@
+use std::fs::create_dir_all;
 use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::io::BufReader;
 use std::io::Write;
+use std::path::Path;
 
 use curl::easy::Easy;
 
+use dirs::home_dir;
+
+fn check_ssh_config(ssh_dir: &Path) -> bool {
+	let mut file = match OpenOptions::new()
+		.read(true)
+		.open(ssh_dir.join("config"))
+	{
+		Ok(file) => file,
+		Err(_) => return false,
+	};
+
+	let to_match = "Include hkconfig";
+	let mut reader = BufReader::new(file);
+    let lines = reader.lines();
+	for line_result in lines {
+		let line = match line_result {
+			Ok(line) => line,
+			Err(_) => return false,
+		};
+
+		if line == to_match {
+			return true;
+		}
+	}
+	return false;
+}
+
 fn main() {
-	// let path = Path::new("/home/cooperc/.ssh/ghdb");
-	// let display = path.display();
+	let ssh_dir = home_dir().unwrap().join(".ssh");
+
+	if !check_ssh_config(&ssh_dir) {
+		println!("heads up: you're missing the line \"Include hkconfig\" from ~/.ssh/config");
+	}
+
+	create_dir_all(ssh_dir.join("hkdb")).unwrap();
+
 	let mut file = OpenOptions::new()
 		.read(true)
 		.write(true)
 		.create(true)
 		.truncate(true)
-		.open("/home/cooperc/.ssh/hkdb/ocf.berkeley.edu").unwrap();
+		.open(ssh_dir.join("hkdb").join("ocf.berkeley.edu")).unwrap();
 
 
 	let mut easy = Easy::new();
